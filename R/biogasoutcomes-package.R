2c802f78b3fe0d5f5d48fe0cc839e90ba6941365
Lars Schöbitz <lschoebitz@ethz.ch>
2023-08-23 14:51:39 +0200

    Write up documentation

---
+++ R/biogasoutcomes-package.R
@@ -0,0 +1,8 @@
+#' @keywords internal
+"_PACKAGE"
+
+#' @aliases biogasoutcomes-package
+
+## usethis namespace: start
+## usethis namespace: end
+NULL
---
+++ R/biogasoutcomes.R
@@ -0,0 +1,14 @@
+#' Qualitative interviews about the outcomes of biogas projects in Malawi
+#'
+#' TODO: More detailed description
+#'
+#' @format A tibble with 259 rows and 5 variables:
+#' \describe{
+#'   \item{interview_id}{ The site id where the interview was performed.}
+#'   \item{interview_date}{ The date on which the interview was performed.}
+#'   \item{question_id}{ The number of the question within each interview. Note: Number of questions differ between interviews.}
+#'   \item{interviewer}{ The question asked by the interviewer.}
+#'   \item{interviewee}{ The answer given by the interviewee.}
+#' }
+#'
+"biogasoutcomes"
