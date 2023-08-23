33e1e5e721d428eebd59714bf7516764c1eed4f5
Lars Schöbitz <lschoebitz@ethz.ch>
2023-08-23 13:22:18 +0200

    Read data as for loop

--- R/convert-to-df.R
+++ R/convert-to-df.R
@@ -1,4 +1,8 @@
 
+library(dplyr)
+library(tidyr)
+library(purrr)
+library(stringr)
 
 # functions ---------------------------------------------------------------
 
@@ -20,35 +24,57 @@ rectangularize <- function(df) {
 
 # https://www.perplexity.ai/search/cab027ef-a5ef-4b2c-9201-0c990a9dc953?s=c
 
+files <- list.files(path = "data", pattern = "*.md", full.names = TRUE)
+
+markdown_text <- list()
+
+for (i in seq_along(files)) {
+    markdown_text[[i]] <- readLines(files[[i]])
+}
+
 # Read the markdown file
-markdown_text <- readLines("data/01_2021-05-31.md")
+# markdown_text <- readLines("data/01_2021-05-31.md")
 
 # Initialize an empty vector for the text strings
 text_strings <- c()
 
+# Initialize an empty list for the text string vectors
+text_strings_list <- list()
+
 # Initialize a variable to store the current text string
 current_string <- ""
 
-# Iterate through the lines of the markdown file
-for (line in markdown_text) {
-    # If the line is not empty, add it to the current text string
-    if (line != "") {
-        current_string <- paste(current_string, line)
-    } else {
-        # If the line is empty, add the current text string to the vector
-        text_strings <- c(text_strings, current_string)
-        # Reset the current text string
-        current_string <- ""
+for (i in seq_along(files)) {
+    markdown_text[[i]] <- readLines(files[[i]])
+    # Iterate through the lines of the markdown file
+    for (line in markdown_text[[i]]) {
+        # If the line is not empty, add it to the current text string
+        if (line != "") {
+            current_string <- paste(current_string, line)
+        } else {
+            # If the line is empty, add the current text string to the vector
+            text_strings <- c(text_strings, current_string)
+            # Reset the current text string
+            current_string <- ""
+        }
     }
+    text_strings_list[[i]] <- text_strings
 }
+
+tibble::tibble(text = text_strings_list[[1]]) |> View()
+
+
+
+# trial -------------------------------------------------------------------
+
+
+# trial end ---------------------------------------------------------------
+
+
 
 # Create a one-column tibble with the text strings
 
 text_tibble <- tibble::tibble(text = text_strings)
-
-library(dplyr)
-library(tidyr)
-library(purrr)
 
 tib <- text_tibble |> 
     mutate(interviewer = case_when(
@@ -77,23 +103,19 @@ if (length(index_interviewee) > 0) {
 
 # -------------------------------------------------------------------------
 
-
 tib2 <- tib |> 
     pivot_longer(cols = !text,
                  names_to = "person",
                  values_to = "value") |> 
     filter(value == TRUE) |> 
     select(-value) |> 
-    #mutate(id = seq(1:n())) |>
     mutate(text = str_remove(text, fixed(" **Interviewer:** "))) |> 
     mutate(text = str_remove(text, fixed(" **Interviewee:** ")))
 
 
 # -------------------------------------------------------------------------
 
-
-
-tib2 |> 
+tib3 <- tib2 |> 
     pivot_wider(names_from = person,
                 values_from = text, 
                 values_fn = list) |> 
@@ -102,7 +124,7 @@ tib2 |>
 
 # -------------------------------------------------------------------------
 
-tib2 |> 
+tib4 <- tib3 |> 
     mutate(interview_date = lubridate::ymd("2021-05-31"),
            interview_id = 1,
            question_id = seq(1:n()))
